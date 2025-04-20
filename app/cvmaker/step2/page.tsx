9c335ef60e5fe00658454635b43cff1a6514a28a
dindinmhs <dindin.imanudin@mhs.itenas.ac.id>
2025-04-20 19:07:30 +0700

    cvmaker 3 final

--- app/cvmaker/step2/page.tsx
+++ app/cvmaker/step2/page.tsx
@@ -9,17 +9,22 @@ const Step2Page = () => {
             <div className="mt-16">
                 <h3 className="text-center text-4xl text-[#3582C5] font-bold">Hubungkan ke profilmu</h3>
                 <div className="max-w-3xl rounded-2xl mx-auto overflow-hidden mt-8">
-                    <button className="flex w-full justify-between items-center p-8 bg-[#E7F1F7]">
+                    <button className="flex w-full justify-between items-center p-8 bg-[#E7F1F7] font-medium text-lg">
                         <span>Facebook</span>
                         <FaArrowRight/>
                     </button>
-                    <button className="flex w-full justify-between items-center p-8 bg-[#E7F1F7]">
-                        <span>Facebook</span>
+                    <button className="flex w-full justify-between items-center p-8 bg-[#E7F1F7] border-y-2 border-[#7C9EB2] font-medium text-lg">
+                        <span>Google</span>
                         <FaArrowRight/>
                     </button>
-                    <button className="flex w-full justify-between items-center p-8 bg-[#E7F1F7]">
-                        <span>Facebook</span>
+                    <button className="flex w-full justify-between items-center p-8 bg-[#E7F1F7] font-medium text-lg">
+                        <span>Linkedin</span>
                         <FaArrowRight/>
+                    </button>
+                </div>
+                <div className="flex">
+                    <button className="bg-[#3785C6] px-4 py-2 rounded-2xl text-xl font-semibold text-white mx-auto mt-10">
+                        Selanjutnya
                     </button>
                 </div>
             </div>
