93209aac1e73b721cad2ed365d39615a77e2aaeb
Cleosazelda <cleosazeldaa@gmail.com>
2025-04-20 12:50:10 +0700

    home & footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import { Footer } from "@/components/common";
+import { BeritaSection, HeroSection, LayananSection, TestimoniSection } from "@/components/home";
+
 export default function Home() {
   return (
-    <main className="container mx-auto py-8">
-      {/* Empty home page with just navbar (navbar is in layout) */}
+    <main className="">
+      <HeroSection/>
+      <LayananSection/>
+      <div className="relative">
+        <TestimoniSection/>
+        <BeritaSection/>
+        <div className="absolute top-0 left-0 w-full h-full -z-10 overflow-hidden">
+              <div className="absolute top-[-150px] left-[-100px] w-[300px] h-[300px] bg-blue-500 opacity-30 rounded-full blur-3xl"></div>
+              <div className="absolute top-[-150px] right-[-100px] w-[300px] h-[300px] bg-blue-500 opacity-30 rounded-full blur-3xl"></div>
+              <div className="absolute bottom-[-100px] left-[-100px] w-[300px] h-[300px] bg-blue-500 opacity-30 rounded-full blur-3xl"></div>
+              <div className="absolute bottom-[300px] right-[100px] w-[300px] h-[300px] bg-blue-500 opacity-30 rounded-full blur-3xl"></div>
+          </div>
+      </div>
+      <Footer/>
     </main>
   );
 }
--- components/common/index.ts
+++ components/common/index.ts
@@ -1,2 +1,3 @@
 export * from './navbar'
 export * from './profile'
+export * from './footer'
